47d23262000858f8365c7bd5ae7bcccaab693791
Sanjana Chawla <sanjanachawla99@gmail.com>
2024-02-18 13:05:36 -0800

    Tutorial: made button open up image library

--- App.js
+++ App.js
@@ -3,17 +3,30 @@ import { StyleSheet, View } from 'react-native';
 
 import Button from './components/Button';
 import ImageViewer from './components/ImageViewer';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" />
+        <Button theme="primary" label="Choose a photo"  onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
